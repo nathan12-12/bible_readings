96a354dc8861bd9c87c041459c5f34c3f6e6eb3e
nathan12-12 <na69than@gmail.com>
2025-12-01 23:43:45 -0600

    Add files via upload

---
+++ lib/bible_plan.dart
@@ -0,0 +1,116 @@
+const Map<String, List<String>> dailyBibleReadings = {
+  // January
+  // 1st Week
+  'Jan 1': ['Genesis 1', 'Genesis 2', 'Psalms 1', 'Psalms 2', 'Matthew 1', 'Matthew 2'],
+  'Jan 2': ['Genesis 3', 'Genesis 4', 'Psalms 3', 'Psalms 4', 'Psalms 5', 'Matthew 3', 'Matthew 4'],
+  'Jan 3': ['Genesis 5', 'Genesis 6', 'Psalms 6', 'Psalms 7', 'Psalms 8', 'Matthew 5'],
+  'Jan 4': ['Genesis 7', 'Genesis 8', 'Psalms 9', 'Psalms 10', 'Matthew 6'],
+  'Jan 5': ['Genesis 9', 'Genesis 10', 'Psalms 11', 'Psalms 12', 'Psalms 13', 'Matthew 7'],
+  'Jan 6': ['Genesis 11', 'Genesis 12', 'Psalms 14', 'Psalms 15', 'Psalms 16', 'Matthew 8'],
+  'Jan 7': ['Genesis 13', 'Genesis 14', 'Psalms 17', 'Matthew 9'],
+  // 2nd Week
+  'Jan 8': ['Genesis 15', 'Genesis 16', 'Psalm 18', 'Matthew 10'],
+  'Jan 9': ['Genesis 17', 'Genesis 18', 'Psalms 19', 'Psalms 20', 'Psalms 21', 'Matthew 11'],
+  'Jan 10': ['Genesis 19', 'Psalms 22', 'Matthew 12'],
+  'Jan 11': ['Genesis 20', 'Genesis 21', 'Psalms 23', 'Psalms 24', 'Psalms 25', 'Matthew 13'],
+  'Jan 12': ['Genesis 22', 'Genesis 23', 'Psalms 26', 'Psalms 27', 'Psalms 28', 'Matthew 14'],
+  'Jan 13': ['Genesis 24', 'Psalms 29', 'Psalms 30', 'Matthew 15'],
+  'Jan 14': ['Genesis 25', 'Genesis 26', 'Psalms 31', 'Matthew 16'],
+  // 3rd Week
+  'Jan 15': ['Genesis 27', 'Psalms 32', 'Matthew 17'],
+  'Jan 16': ['Genesis 28', 'Genesis 29', 'Pslams 33', 'Matthew 18'],
+  'Jan 17': ['Genesis 30', 'Psalms 34', 'Matthew 19'],
+  'Jan 18': ['Genesis 31', 'Psalms 35', 'Matthew 20'],
+  'Jan 19': ['Genesis 32', 'Genesis 33', 'Psalms 36', 'Matthew 21'],
+  'Jan 20': ['Genesis 34', 'Genesis 35', 'Psalms 37', 'Matthew 22'],
+  'Jan 21': ['Genesis 36', 'Psalms 38', 'Matthew 23'],
+  // 4th Week
+  'Jan 22': ['Genesis 37', 'Psalms 39', 'Psalms 40', 'Matthew 24'],
+  'Jan 23': ['Genesis 38', 'Psalms 41', 'Psalms 42', 'Psalms 43', 'Matthew 25'],
+  'Jan 24': ['Genesis 39', 'Genesis 40', 'Psalms 44', 'Matthew 26'],
+  'Jan 25': ['Genesis 41', 'Psalms 45', 'Matthew 27'],
+  'Jan 26': ['Genesis 42', 'Genesis 43', 'Psalms 46', 'Psalms 47', 'Psalms 48', 'Matthew 28'],
+  'Jan 27': ['Genesis 44', 'Genesis 45', 'Psalms 49', 'Romans 1', 'Romans 2'],
+  'Jan 28': ['Genesis 46', 'Genesis 47', 'Psalms 50', 'Romans 3', 'Romans 4'],
+  // Last Week
+  'Jan 29': ['Genesis 48', 'Genesis 49', 'Genesis 50', 'Psalms 51', 'Psalms 52', 'Romans 5', 'Romans 6'],
+  'Jan 30': ['Exodus 1', 'Exodus 2', 'Psalms 53', 'Psalms 54', 'Psalms 55', 'Romans 7', 'Romans 8'],
+  'Jan 31': ['Exodus 3', 'Exodus 4', 'Psalms 56', 'Psalms 57', 'Romans 9'],
+
+  // February
+  // 1st Week
+  'Feb 1': ['Exodus 5', 'Exodus 6', 'Psalms 58', 'Psalms 59', 'Romans 10', 'Romans 11'],
+  'Feb 2': ['Exodus 7', 'Exodus 8', 'Psalms 60', 'Psalms 61', 'Romans 12'],
+  'Feb 3': ['Exodus 9', 'Pslams 62', 'Psalms 63', 'Romans 13', 'Romans 14'],
+  'Feb 4': ['Exodus 10', 'Pslams 64', 'Psalms 65', 'Romans 15', 'Romans 16'],
+  'Feb 5': ['Exodus 11', 'Exodus 12', 'Psalms 66', 'Psalms 67', 'Mark 1'],
+  'Feb 6': ['Exodus 13', 'Exodus 14', 'Psalms 68', 'Mark 2'],
+  'Feb 7': ['Exodus 15', 'Psalms 69', 'Mark 3'],
+  // 2nd Week
+  'Feb 8': ['Exodus 16', 'Psalms 70', 'Psalms 71', 'Mark 4'],
+  'Feb 9': ['Exodus 17', 'Exodus 18', 'Psalms 72', 'Mark 5'],
+  'Feb 10': ['Exodus 19', 'Exodus 20', 'Psalms 73', 'Mark 6'],
+  'Feb 11': ['Exodus 21', 'Psalms 74', 'Mark 7'],
+  'Feb 12': ['Exodus 22', 'Psalms 75', 'Psalms 76', 'Mark 8'],
+  'Feb 13': ['Exodus 23', 'Psalms 77', 'Mark 9'],
+  'Feb 14': ['Exodus 24', 'Exodus 25', 'Psalms 78', 'Mark 10'],
+  // 3rd Week
+  'Feb 15': ['Exodus 26', 'Psalms 79', 'Psalms 80', 'Mark 11'],
+  'Feb 16': ['Exodus 27', 'Psalms 81', 'Psalms 82', 'Mark 12'],
+  'Feb 17': ['Exodus 28', 'Psalms 83', 'Psalms 84', 'Mark 13'],
+  'Feb 18': ['Exodus 29', 'Psalms 85', 'Psalms 86', 'Mark 14'],
+  'Feb 19': ['Exodus 30', 'Psalms 87', 'Psalms 88', 'Mark 15', 'Mark 16'],
+  'Feb 20': ['Exodus 31', 'Exodus 32', 'Psalms 89', '1 Corinthians 1', '1 Corinthians 2'],
+  'Feb 21': ['Exodus 33', 'Exodus 34', 'Psalms 90', 'Psalms 91', '1 Corinthians 3'],
+  // 4th Week
+  'Feb 22': ['Exodus 35', 'Psalms 92', 'Psalms 93', '1 Corinthians 4', '1 Corinthians 5'],
+  'Feb 23': ['Exodus 36', 'Psalms 94', 'Psalms 95', '1 Corinthians 6'],
+  'Feb 24': ['Exodus 37', 'Psalms 96', 'Psalms 97', 'Psalms 98', 'Psalms 99' '1 Corinthians 7'],
+  'Feb 25': ['Exodus 38', 'Psalms 100', 'Psalms 101', '1 Corinthians 8', '1 Corinthians 9'],
+  'Feb 26': ['Exodus 39', 'Exodus 40', 'Psalms 102', '1 Corinthians 10'],
+  'Feb 27': ['Leviticus 1', 'Leviticus 2', 'Psalms 103', '1 Corinthians 11'],
+  'Feb 28': ['Leviticus 3', 'Leviticus 4', 'Psalms 104', '1 Corinthians 12', '1 Corinthians 13'],
+  'Feb 29': ['Bro. Roberts did not ... ', '... account for leap year, so ... ', ' ... read your favorite chapters'], // Leap year day
+
+  // November
+  'Nov 24': ['Nehemiah 12', 'Amos 4', '2 Timothy 1'],
+
+
+  // December
+  // 1st Week
+  'Dec 1': ['Esther 9', 'Esther 10', 'Jonah 1', 'Hebrews 6', 'Hebrews 7'],
+  'Dec 2': ['Job 1', 'Job 2', 'Jonah 2', 'Jonah 3', 'Hebrews 8', 'Hebrews 9'],
+  'Dec 3': ['Job 3', 'Job 4', 'Jonah 4', 'Hebrews 10'],
+  'Dec 4': ['Job 5', 'Micah 1', 'Hebrews 11'],
+  'Dec 5': ['Job 6', 'Job 7', 'Micah 2', 'Hebrews 12'],
+  'Dec 6': ['Job 8', 'Micah 3', 'Micah 4', 'Hebrews 13'],
+  'Dec 7': ['Job 9', 'Micah 5', 'James 1'],
+  // 2nd Week
+  'Dec 8': ['Job 10', 'Micah 6', 'James 2'],
+  'Dec 9': ['Job 11', 'Micah 7', 'James 3', 'James 4'],
+  'Dec 10': ['Job 12', 'Nahum 1', 'Nahum 2', 'James 5'],
+  'Dec 11': ['Job 13', 'Nahum 3', '1 Peter 1'],
+  'Dec 12': ['Job 14', 'Habakkuk 1', '1 Peter 2'],
+  'Dec 13': ['Job 15', 'Habakkuk 2', '1 Peter 3', '1 Peter 4', '1 Peter 5'],
+  'Dec 14': ['Job 16', 'Job 17', 'Habakkuk 3', '2 Peter 1', '2 Peter 2'],
+  // 3rd Week
+  'Dec 15': ['Job 18', 'Job 19', 'Zephaniah 1', '2 Peter 3'],
+  'Dec 16': ['Job 20', 'Zephaniah 2', '1 John 1', '1 John 2'],
+  'Dec 17': ['Job 21', 'Zephaniah 3', '1 Johm 3', '1 John 4'],
+  'Dec 18': ['Job 22', 'Haggai 1', 'Haggai 2', '1 John 5'],
+  'Dec 19': ['Job 23', 'Job 24', 'Zechariah 1', '2 John 1', '3 John 1'],
+  'Dec 20': ['Job 25', 'Job 26', 'Job 27', 'Zechariah 2', 'Zechariah 3', 'Jude 1'],
+  'Dec 21': ['Job 28', 'Zechariah 4', 'Zechariah 5', 'Revelation 1', 'Revelation 2'],
+  // 4th Week
+  'Dec 22': ['Job 29', 'Job 30', 'Zechariah 6', 'Zechariah 7', 'Revelation 3', 'Revelation 4'],
+  'Dec 23': ['Job 31', 'Job 32', 'Zechariah 8', 'Revelation 5', 'Revelation 6'],
+  'Dec 24': ['Job 33', 'Zechariah 9', 'Revelation 7', 'Revelation 8', 'Revelation 9'],
+  'Dec 25': ['Job 34', 'Zechariah 10', 'Revelation 10', 'Revelation 11'],
+  'Dec 26': ['Job 35', 'Job 36', 'Zechariah 11', 'Revelation 12', 'Revelation 13'],
+  'Dec 27': ['Job 37', 'Zechariah 12', 'Revelation 14'],
+  'Dec 28': ['Job 38', 'Zechariah 13', 'Zechariah 14', 'Revelation 15', 'Revelation 16'],
+  // Last Week
+  'Dec 29': ['Job 39', 'Malachi 1', 'Revelation 17', 'Revelation 18'],
+  'Dec 30': ['Job 40', 'Malachi 2', 'Revelation 19', 'Revelation 20'],
+  'Dec 31': ['Job 41', 'Job 42', 'Malachi 3', 'Malachi 4', 'Revelation 21', 'Revelation 22'],
+};
